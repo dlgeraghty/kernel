b480c060230de6d0c630fed830c552b67c4813e5
Carlos Bilbao <Zildj1an@users.noreply.github.com>
2019-06-15 15:53:50 +0200

    Update kernel.asm

--- kernel.asm
+++ kernel.asm
@@ -10,4 +10,7 @@ extern kmain 		;this function is going to be located on our c code (kernel.c)
 start:
 	cli		;clears the interrupts
 	call kmain	;send processor to continue execution from the kmain function in c code
+	; we should never return
+.idle:
 	hlt		;halt the cpu(pause it from executing from this address)
+	jmp .idle
