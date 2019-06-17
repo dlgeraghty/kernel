f11bb548a84b9eef80d528a59b7286af728eb81a
Carlos Bilbao <Zildj1an@users.noreply.github.com>
2019-06-17 13:45:14 +0200

    inlines

--- kernel.c
+++ kernel.c
@@ -22,43 +22,43 @@ void printc(char l, char c, int *x, int *y, unsigned char *v){
 
 }
 
-void cls(unsigned char *v){
+inline void cls(unsigned char *v){
   for (int i = 0; i < w*h*2; i++) {
     v[i] = 0x0;
   }
 }
 
-void prints(char *p, int *x, int *y, unsigned char *v){
+inline void prints(char *p, int *x, int *y, unsigned char *v){
   for(int i = 0; p[i] != '\0'; ++i){
     printc(p[i], 0x09, x, y, v);
   }
 }
 
-int inport(unsigned short port){
+inline int inport(unsigned short port){
   int rv;
   __asm__ __volatile__ ("inb %1, %0" : "=a" (rv) : "dN" (port));
   return rv;
 }
 
-void outport(unsigned short port, unsigned short data){
+inline void outport(unsigned short port, unsigned short data){
   __asm__ __volatile__ ("out %1, %0" : : "dN" (port), "a" (data));
 }
 
-void printcursor(int x,int y){
+inline void printcursor(int x,int y){
   outport(0x3D4, 14);
   outport(0x3D5, (y*w + x) >> 8);
   outport(0x3D4, 15);
   outport(0x3D5, (y*w + x));
 }
 
-void nextLine(){
+inline void nextLine(){
   string vidmem = (string)0xb8000;
   for(int i = 0; i < w*(h-1)*2; i++){
     vidmem[i] = vidmem[i + w*2];
   }
 }
 
-int strlength(string s){
+inline int strlength(string s){
   int l = 0;
   while(s[l] != '\0') l++;
   return l;
